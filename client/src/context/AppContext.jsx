bad6eeb36442b492eafd7ab9ad0727c5dcbcd15a
Sankalp-Space <sankalpshrivastav2001@gmail.com>
2025-04-24 00:02:31 +0530

    Login Page and some motion added for impression

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,11 +3,12 @@ import { createContext,useState } from "react";
 export const AppContext=createContext()
 
 const AppContextprovider=(props)=>{
+    const [showLogin, setShowLogin]=useState(false)
     
     const [user, setUser]= useState(null);
 
     const value ={
-        user, setUser
+        user, setUser, showLogin,setShowLogin
     }
     return (
         <AppContext.Provider value={value}>
