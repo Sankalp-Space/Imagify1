ecd95c2373b7dc5e7ec4878c5bfbc12fcaef13d4
Sankalp-Space <sankalpshrivastav2001@gmail.com>
2025-04-26 18:34:53 +0530

    Authentication done and Clipdrop Api intergrated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -19,11 +19,55 @@ const registerUser= async(req,res)=>{
 
         const newUser= new userModel(userData);
         const user = await newUser.save()
-        const token = jwt.sign({id : user._id})
+        const token = jwt.sign({id : user._id}, process.env.JWT_SECRET)
+
+        res.json({succes:true , token, user:{name : user.name}})
 
     }
 
     catch(error){
-
+        console.log(error)
+        res.json({succes:false, message: error.message})
     }
 }
+
+
+const loginUser = async(req,res)=>{
+    try{
+        const {email, password}= req.body;
+        const user = await userModel.findOne({email})
+
+        if(!user){
+            return  res.json({success:false, message:' User does not exist'})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(isMatch){
+            const token = jwt.sign({id : user._id}, process.env.JWT_SECRET)
+
+            res.json({succes:true , token, user:{name : user.name}})
+        }
+        else{
+            return res.json({success:false, message:'Invalid credentials'})
+        }
+    }
+    catch(error){
+        console.log(error)
+        res.json({succes:false, message: error.message})
+    }
+}
+
+const userCredits= async(req,res)=>{
+    try{
+        const {userId}=req.body;
+        const user = await userModel.findById(userId)
+        res.json({success:true, credits : user.creditBalance, user : { name: user.name}})
+
+    }
+    catch(error){
+        console.log(error)
+        res.json({success:false, message: error.message})
+    }
+}
+
+export {registerUser, loginUser, userCredits}
